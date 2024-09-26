0d31c36adc3535d5d529029cf265115b301f49e9
Himanshu Rawat <himanshuu004@gmail.com>
2024-09-26 17:32:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var input = document.querySelector("#search-bar");
+var data = [
+    {
+    narm:"himanshu" , src:"https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXQlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D"},
+    
+    {narm:"hima" , src:"https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXQlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D"},
+    
+    {narm:"himan" , src:"https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXQlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D"},
+   
+    {narm:"Sahil" , src:"https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXQlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D"},
+    
+    {narm:"Priyanshu" , src:"https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXQlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D"
+
+  },
+  {narm:"himanshu" , src:"https://plus.unsplash.com/premium_photo-1690407617542-2f210cf20d7e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXQlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D"
+
+  }
+
+]
+
+var card = "";
+
+
+data.forEach(function(elem){
+
+  card +=` <div class="card">
+        <img src="${elem.src}" alt="">
+        <h4>${elem.narm}</h4>
+      </div>`
+
+});
+
+document.querySelector(".content").innerHTML = card;
+
+input.addEventListener("input",function(){
+  var matching = data.filter(function(e){
+    return e.narm.startsWith(input.value);
+  })
+
+  var newUser = "";
+  matching.forEach(function(elem){
+
+    newUser +=` <div class="card">
+          <img src="${elem.src}" alt="">
+          <h4>${elem.narm}</h4>
+        </div>`
+  
+  });
+  document.querySelector(".content").innerHTML = newUser;
+
+});
